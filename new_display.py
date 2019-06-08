535b7800cce4301f71b3b7b0a0fdaead2163375f
Ace Gallagher <ace@blck-tux.loc>
2019-06-08 16:56:36 -0600

    really tho

--- new_display.py
+++ new_display.py
@@ -147,43 +147,6 @@ class Menu:
                     
                 return
 
-	    elif GPIO.event_detected(key['key2']): # key2 is a selection, follow the action/submenu selected
-
-			if entryIsAction==True: # call function that entry describes
-
-			else: # display submenu
-
-
-			#action loop
-			while done==0:
-				time.sleep(.05)
-
-				if GPIO.event_detected(key['down']):
-					#pos=pos+1
-					apos=posDown(apos,3)
-	                                dispListMenu(device,title,mlist,alist,pos,0,vpos) 
-
-				elif GPIO.event_detected(key['up']):
-					#pos=pos+1
-					apos=posUp(apos,3)
-	                                dispListMenu(device,title,mlist,alist,pos,0,vpos) 
-
-				elif GPIO.event_detected(key['key2']):
-					actionhandler(device,pos+vpos,apos,mname,vpos)
-					apos=0
-					done=1
-
-				# back exit
-				elif GPIO.event_detected(key['key1']):
-					done=1
-					apos=0
-
-                        dispListMenu(device,title,mlist,alist,pos,0,vpos)
-
-		#// EXIT STRATEGY
-		elif GPIO.event_detected(key['key1']):
-			if exit==True:
-				return
 
 class Action: 
 
